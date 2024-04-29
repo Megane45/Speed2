8560bf6451c6ea0732376b0e8d0087bf9efedd4c
Megane <megane.loisele@gmail.com>
2024-04-29 12:46:25 +0200

    modification de la data ajout d'un style css a mes cartes film

--- src/Components/Film.jsx
+++ src/Components/Film.jsx
@@ -1,11 +1,16 @@
+import "./film.css";
 function Film({ data }) {
   return (
-    <div>
-      <h3>{data[0].Nom}</h3>
-      <p>{data[0].description}</p>
-      <img src={data[0].image} alt={data[0].Nom} />
-      <p>{data[0].dateDeSortie}</p>
-    </div>
+    <section className="card">
+      <h2>Films</h2>
+      <div className="Films">
+        <h3>{data[0].Nom}</h3>
+        <img src={data[0].image} alt={data[0].Nom} />
+        <p>{data[0].description}</p>
+
+        <p>{data[0].dateDeSortie}</p>
+      </div>
+    </section>
   );
 }
 
